147e605976d9b87d7902d79aa006a193469ccd29
MFLind <Madde@quaggatech.com>
2022-12-05 08:44:09 +0000

    Making a Request Part 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-B6bOoVoxbhZ90j4pZ6UtwdGeTzY
+const API_KEY = "B6bOoVoxbhZ90j4pZ6UtwdGeTzY";
+const API_KEY = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_KEY}?api_key={API_KEY}`
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
