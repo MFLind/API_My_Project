dbacf882d8c188140d3a72d4872a138783702d2b
MFLind <Madde@quaggatech.com>
2022-12-05 12:21:01 +0000

    Making a Request part 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,31 @@
 const API_KEY = "B6bOoVoxbhZ90j4pZ6UtwdGeTzY";
-const API_KEY = "https://ci-jshint.herokuapp.com/api";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
-    const queryString = `${API_KEY}?api_key={API_KEY}`
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
 
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displyStatus(data);
+    } else {
+        throw new Error(data.error);
     }
+}
+
+function displyStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+    
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
 }
